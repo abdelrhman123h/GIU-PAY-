855902d1546d4c8741ee5a1421a0e21f708924fb
abdelrhman123h <168747672+abdelrhman123h@users.noreply.github.com>
2025-07-31 02:15:36 +0300

    yes

--- e-wallet backend/ewallet-backend/src/main.ts
+++ e-wallet backend/ewallet-backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS
+  app.enableCors({
+  origin: 'https://giu-pay.vercel.app',
+});
   await app.listen(process.env.PORT ?? 3333);
 }
 
